b0217070ee50356f8a4c00b838b9774d9e21e40a
236-Sowmya <s.sowmya9494@gmail.com>
2022-01-03 10:19:48 +0530

    Add project files.

---
+++ MVC- ACCOUNT/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC__ACCOUNT.Models;
+
+namespace MVC__ACCOUNT.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Account> accounts = new List<Account>();
+            accounts.Add(new Account() { ID = 203, AccountHolderName = "Sowmya", AccountHolderAddress = "Subhash Road, Anantapur" });
+            accounts.Add(new Account() { ID = 204, AccountHolderName = "Chinni", AccountHolderAddress = "Ramachandra Nagar, Anantaput" });
+            accounts.Add(new Account() { ID = 205, AccountHolderName = "Arun Kumar", AccountHolderAddress = "KLD Bypass, Anantapur" });
+            ViewData["accounts"] = accounts;
+            return View();
+        }
+    }
+}
---
+++ MVC- ACCOUNT/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC__ACCOUNT.Models
+{
+    public class Account
+    {
+        public int ID { get; set; }
+        public string AccountHolderName { get; set; }
+
+        public string AccountHolderAddress { get; set; }
+    }
+}
